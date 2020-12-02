6ebe41cb725bc5e5bf94fbd7e80f246319ba88d0
Ognegin Kirill <ogneginlabs@gmail.com>
2020-12-02 12:38:09 +0700

    Конвертировал объект murzik в Json. Вывел на экран результат

--- lab4_sand_club/app/src/main/java/com/example/lab4_sand_club/MainActivity.java
+++ lab4_sand_club/app/src/main/java/com/example/lab4_sand_club/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -15,12 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView output = findViewById(R.id.outputTView);
+        View color = findViewById(R.id.colorCat);
+
         Cat murzik = new Cat();
         murzik.name = "Мурзик";
         murzik.age = 9;
-        murzik.color = Color.RED;
+        murzik.color = Color.GREEN;
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
+        Log.i("GSON", gson.toJson(murzik));
+        output.setText("Кот: " + murzik.name+ "," + murzik.age + "лет, цвет:");
+        color.setBackgroundColor(murzik.color);
 
     }
 }

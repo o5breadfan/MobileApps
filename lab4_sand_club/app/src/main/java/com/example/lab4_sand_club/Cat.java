363d6bb77e499548e18aed112e59d6c6deea8ad9
Ognegin Kirill <56971033+o5breadfan@users.noreply.github.com>
2020-12-02 12:20:53 +0700

    Лабораторная работа 4. Создал класс Cat и подключил библиотеку Gson

---
+++ lab4_sand_club/app/src/main/java/com/example/lab4_sand_club/Cat.java
@@ -0,0 +1,11 @@
+package com.example.lab4_sand_club;
+
+public class Cat {
+    public String name;
+    public int age;
+    public int color;
+
+    public Cat(){
+
+    }
+}
--- lab4_sand_club/app/src/main/java/com/example/lab4_sand_club/MainActivity.java
+++ lab4_sand_club/app/src/main/java/com/example/lab4_sand_club/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.lab4_sand_club;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 9;
+        murzik.color = Color.RED;
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
     }
 }

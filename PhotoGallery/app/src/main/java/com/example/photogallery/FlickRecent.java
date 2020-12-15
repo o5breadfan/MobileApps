9ea757d582f14dc9c6c11dbf0b3172c553ac76d4
Ognegin Kirill <56971033+o5breadfan@users.noreply.github.com>
2020-12-15 15:02:49 +0700

    Завершил работу.

--- PhotoGallery/app/src/main/java/com/example/photogallery/FlickRecent.java
+++ PhotoGallery/app/src/main/java/com/example/photogallery/FlickRecent.java
@@ -6,9 +6,9 @@ import retrofit2.http.Query;
 import com.example.photogallery.Response;
 
 public interface FlickRecent {
-    @GET("services/rest/?method=flickr.photos.getRecent&api_key=2429f208737a3d16d5b6118bd2b75378&extras=url_s&per_page=4&page=1&format=json&nojsoncallback=1")
+    @GET("services/rest/?method=flickr.photos.getRecent&api_key=2429f208737a3d16d5b6118bd2b75378&extras=url_s&per_page=20&page=1&format=json&nojsoncallback=1")
     Call<Response> getRecent();
 
-    @GET("services/rest/?method=flickr.photos.search&api_key=2429f208737a3d16d5b6118bd2b75378&extras=url_s&per_page=4&page=1&format=json&nojsoncallback=1")
+    @GET("services/rest/?method=flickr.photos.search&api_key=2429f208737a3d16d5b6118bd2b75378&extras=url_s&per_page=20&page=1&format=json&nojsoncallback=1")
     Call<Response> getSearchPhotos(@Query("text") String keyWord);
 }
